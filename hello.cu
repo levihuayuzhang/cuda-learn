fdef28f12db775865662077f696d535cded5cfba
Huayu Zhang <zhanghuayu.dev@gmail.com>
2023-10-12 16:04:01 +0800

    matrix multiply using gpu blocks

--- hello.cu
+++ hello.cu
@@ -1,34 +1,51 @@
 #include <stdio.h>
-__global__ void multi(int *a, int *b, int *res){
-    *res = (*a) * (*b);
+#include <iostream>
+#include <cuda.h>
+#define N 512
+
+__global__ void multi(int *a, int *b, int *c){
+    c[blockIdx.x] = a[blockIdx.x] * b[blockIdx.x];
+}
+
+//populate vectors with random ints
+void random_ints(int* a, int count) {
+    for (int i=0; i < count; i++){
+        a[i] = rand() % 1000;
+    }
 }
 
 int main(void) {
-    int a, b, c; // host copies
+    int *a, *b, *c; // host copies
     int *d_a, *d_b, *d_c; // device copies
-    int size = sizeof(int);
+    int size = N * sizeof(int); // array size
 
     // allocate memory for device copies
     cudaMalloc((void**)&d_a, size);
     cudaMalloc((void**)&d_b, size);
     cudaMalloc((void**)&d_c, size);
-
-    a= 3;
-    b= 4;
+    // memory for host copies
+    a = (int*)malloc(size); random_ints(a, N);
+    b = (int*)malloc(size); random_ints(b, N);
+    c = (int*)malloc(size);
 
     // copy host intput value to device
-    cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
-    multi<<<1,1>>>(d_a, d_b, d_c);
+    multi<<<N,1>>>(d_a, d_b, d_c); // launch kernel on GPU with N blocks
 
-    cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
 
+    for (int i = 0; i < N; i++) {
+        printf("Result for c[%d] is %d.\n", i, c[i]);
+    }
+
+    free(a);
+    free(b);
+    free(c);
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
-
-    printf("Hello world! Result is %d", c);
 
     return 0;
 }
