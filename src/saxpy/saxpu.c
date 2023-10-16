5b6343781cf798889440ebed00be329dc235e8bd
Huayu Zhang <zhanghuayu.dev@gmail.com>
2023-10-17 00:06:01 +0800

    saxpu pure c impl

---
+++ src/saxpy/saxpu.c
@@ -0,0 +1,38 @@
+/**
+ * Single-Precision A·X Plus Y in C
+ * 
+ * 2012: https://developer.nvidia.com/blog/six-ways-saxpy/
+ * 2021: https://developer.nvidia.com/blog/n-ways-to-saxpy-demonstrating-the-breadth-of-gpu-programming-options/
+ * 
+ * */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+float* saxpy_cpu(int n, float a, float *x, float *y)
+{
+	for (int i = 0; i < n; ++i)
+		y[i] = a * x[i] + y[i];
+
+    return y;
+}
+
+int main(void) {
+    const int n = 1<<15;
+
+    float x[n];
+    float y[n];
+
+    for (int i = 0; i < n; i++) {
+        x[i] = (float)rand()/(float)(RAND_MAX/3);
+        y[i] = (float)rand()/(float)(RAND_MAX/2);
+    }
+
+    float* res = saxpy_cpu(n, 2.0, x, y);
+
+    for (int i = 0; i < n; i++) {
+        printf("%f\n", res[i]);
+    }
+
+    return 0;
+}
