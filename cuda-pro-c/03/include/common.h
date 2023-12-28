0d5f874e73f60162b9c214354fdae6bfb6ae270d
Huayu Zhang <zhanghuayu.dev@gmail.com>
2023-12-28 16:54:53 +0800

    backup

--- cuda-pro-c/03/include/common.h
+++ cuda-pro-c/03/include/common.h
@@ -1,15 +1,26 @@
-#include <sys/time.h>
+// #include <sys/time.h>
 
 #ifndef _COMMON_H
 #define _COMMON_H
 
-inline double seconds()
-{
-    struct timeval tp;
-    struct timezone tzp;
-    int i = gettimeofday(&tp, &tzp);
-    return (double)tp.tv_sec + (double)tp.tv_usec * 1.e-6;
+// check cuda error if exist when calling cuda runtime function
+#define CHECK(call)                                                                         \
+{                                                                                           \
+    const cudaError_t error = call;                                                         \
+    if (error != cudaSuccess)                                                               \
+    {                                                                                       \
+        fprintf(stderr, "Error at: %s:%d, ", __FILE__, __LINE__);                           \
+        fprintf(stderr, "error code: %d, reason: %s\n", error, cudaGetErrorString(error));  \
+        exit(1);                                                                            \
+    }                                                                                       \
+}                                                                                           \
 
-}
+// inline double seconds()
+// {
+//     struct timeval tp;
+//     struct timezone tzp;
+//     int i = gettimeofday(&tp, &tzp);
+//     return (double)tp.tv_sec + (double)tp.tv_usec * 1.e-6;
+// }
 
 #endif
--- cuda-pro-c/03/reduce-integer.cu
+++ cuda-pro-c/03/reduce-integer.cu
@@ -2,6 +2,7 @@
 #include <string>
 #include <cuda_runtime.h>
 #include "common.h"
+#include <chrono>
 
 int recursiveReduce(int *data, int const size)
 {
@@ -16,7 +17,37 @@ int recursiveReduce(int *data, int const size)
     return recursiveReduce(data, stride);
 }
 
+/**
+ * @brief Neighbored pair implementation with divergence
+ * 
+ * @param g_idata global input data address
+ * @param d_odata global output data address
+ * @param n data set size
+ *
+ */
+__global__ void reduceNeighbored (int *g_idata, int *g_odata, unsigned int n)
+{
+    unsigned int tid = threadIdx.x;
+	unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
+    // data index for each bloack (local pointer) in global mem
+	int *idata = g_idata + blockIdx.x * blockDim.x;
+
+    // boundary check
+	if (idx >= n) return;
+
+	for (int stride = 1; stride < blockDim.x; stride *= 2)
+	{
+		if ((tid % (2 * stride)) == 0)
+		{
+			idata[tid] += idata[tid+stride];
+		}
+		__syncthreads(); // inner block sync (wait for each block to finish)
+	}
+
+    // store result from each block (1st elements) to out put array
+    if (tid == 0) g_odata[blockIdx.x] = idata[0];
+}
 
 int main(int argc, char const *argv[])
 {
@@ -43,27 +74,67 @@ int main(int argc, char const *argv[])
     int *h_odata = (int *) malloc(grid.x * sizeof(int));
     int *tmp = (int *) malloc(bytes);
 
+    // initialize random data
     for (int i = 0; i < size; i++)
     {
         h_idata[i] = (int)(rand() *0xFF);
-    }
-    
+    }    
     memcpy(tmp, h_idata, bytes);
 
-    double iStart, iElaps;
-    int gpu_sum = 0;
-
+    // double iStart, iElaps;
+    // device mem
     int *d_idata = NULL;
     int *d_odata = NULL;
-    cudaMalloc((void **) &d_idata, bytes);
-    cudaMalloc((void **) &d_idata, grid.x * sizeof(int));
+    CHECK(cudaMalloc((void **) &d_idata, bytes));
+    CHECK(cudaMalloc((void **) &d_odata, grid.x * sizeof(int)));
 
-    iStart = seconds();
+    // cpu reduction
+    auto iStart = std::chrono::high_resolution_clock::now();
     int cpu_sum = recursiveReduce(tmp, size);
-    iElaps = seconds() - iStart;
-    std::cout << "CPU reduce recusice: " << iElaps << "sec cpu_sum: " 
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto iElaps = std::chrono::duration_cast<std::chrono::microseconds>(endTime-iStart);
+    double duration = iElaps.count();
+    std::cout << "\nCPU reduce recusice: " << duration << " microsec cpu_sum: " 
                 << cpu_sum << std::endl;
 
+    int gpu_sum;
+    // kernel 1: reduceNeighboared
+    cudaMemcpy(d_idata, h_idata, bytes, cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    iStart = std::chrono::high_resolution_clock::now();
+
+    reduceNeighbored<<<grid, block>>>(d_idata, d_odata, size);
+    cudaDeviceSynchronize();
+
+    endTime = std::chrono::high_resolution_clock::now();
+    iElaps = std::chrono::duration_cast<std::chrono::microseconds>(endTime-iStart);
+    duration = iElaps.count();
+    
+    cudaMemcpy(h_odata, d_odata, grid.x * sizeof(int), cudaMemcpyDeviceToHost);
+    gpu_sum = 0;
+    for (int i = 0; i < grid.x; i++) gpu_sum += h_odata[i];
+
+    std::cout << "GPU reduce neighbored: " << duration << " microsec gpu_sum: " 
+                << gpu_sum << std::endl;
+    if (gpu_sum != cpu_sum) std::cout << "Test result failed: sum result not match!" << std::endl;
+
+
+    
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    // clean resource
+    free(h_idata); free(h_odata);
+    cudaFree(d_idata); cudaFree(d_odata);
+    cudaDeviceReset();
     
 
     std::cout << "end!!!" << std::endl;
